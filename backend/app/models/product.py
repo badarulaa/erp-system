b859b43b96246f83ab170ab55f32372937eb7667
badarulaa <badarul93@gmail.com>
2026-03-17 05:59:39 +0700

    add models product

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -3,9 +3,13 @@ from sqlalchemy.sql import func
 from app.core.database import Base
 
 class Product(Base):
-  __table_name__ = "product"
+  __tablename__ = "product"
 
   id = Column(Integer, primary_key=True, index=True)
   sku = Column(String(50), unique=True, nullable=False)
   name = Column(String(255), nullable=False)
-  category_id = Column(Integer,  )
+  category_id = Column(Integer)
+  brand_id = Column(Integer)
+  description = Column(Text)
+  created_at = Column(DateTime(timezone=True), server_default=func.now())
+  updated_at = Column(DateTime(timezone=True), onupdate=func.now())
